463cf3ef56b44c8b9ec3f3078441d6704d968b69
TOROSI <thomassimons98@gmail.com>
2023-10-08 16:47:19 +0100

    test commit

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } // if the name is Id or CategoryId then you dont need the data annotation for key. Key is not needed here.
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
