d1ac98bf64c3e09070bbc859b40ba8af931d776e
TOROSI <thomassimons98@gmail.com>
2023-10-10 19:01:37 +0100

    test commit

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Common;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
+        {
+            List<Category> catagories = _context.Categories.ToList();
+            return View(catagories);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
